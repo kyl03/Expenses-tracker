f46fffb5ac96c53693c51081dc467ed305a8878f
Karen Yep <karenyeplee@gmail.com>
2022-12-05 19:37:15 -0600

    código listo para testeo

--- control-gastos.js
+++ control-gastos.js
@@ -1,60 +1,47 @@
-
 const totalElement = document.getElementById("totalAmount");
 const addBtn = document.getElementById("addTransactionBtn");
 const transactionsTable = document.getElementById("transactionsTable");
-
+let lastTransactionId;
+let total;
+let totalIncome;
+let totalExpenses;
 let allTransactionArray = [];
-
-
-
-
-
 
 addBtn.addEventListener("click", addTransaction);
 
-let lastId ;
-let total ;
-let totalIncome ;
-let totalExpenses ;
 function loadLocalData() {
     const myArrayFromLocalStorage = localStorage.getItem('myArray')
     if (myArrayFromLocalStorage && myArrayFromLocalStorage.length) {
         allTransactionArray = JSON.parse(myArrayFromLocalStorage)
-    }
-
-    if (allTransactionArray.length > 0) {
         getTotal();
         getTotalIncome();
         getTotalExpenses();
-        showAllTransactions();
-        lastId = allTransactionArray[allTransactionArray.length - 1].id;
-
-    };
-
+        drawAllTransactions();
+        lastTransactionId = allTransactionArray[allTransactionArray.length - 1].id;
+    }
 }
-function showAllTransactions() {
+function drawAllTransactions() {
     let transactionNumber = 0;
     allTransactionArray.forEach(transaction =>
-        showTransaction(transaction, transactionNumber += 1)
+        drawTransaction(transaction, transactionNumber += 1)
     );
 }
 
 function getTotal() {
     total = updateTotal("total");
-
     if (total > 0) {
         totalElement.style.color = "rgb(59, 228, 59)";
-    } else{
+    } else {
         totalElement.style.color = "red"
-    } 
-    showData(total, totalElement);
+    }
+    drawData(total, totalElement);
 
 }
 
 function getTotalExpenses() {
     let totalExpenses = updateTotal("expenses");
     const totalExpensesElement = document.getElementById("totalExpenses");
-    showData(totalExpenses, totalExpensesElement);
+    drawData(totalExpenses, totalExpensesElement);
 }
 
 function updateTotal(transactionType) {
@@ -81,35 +68,32 @@ function updateTotal(transactionType) {
 function getTotalIncome() {
     const totalIncomeElement = document.getElementById("totalIncome");
     totalIncome = updateTotal("income");
-    showData(totalIncome, totalIncomeElement);
+    drawData(totalIncome, totalIncomeElement);
 }
 
-function showData(value, element) {
-
+function drawData(value, element) {
     element.innerText = value;
-
 }
-function showTransaction(transaction, arrayPosition) {
+
+function drawTransaction(transaction) {
     const transactionElement = document.createElement("tr");
     transactionElement.setAttribute("id", transaction.id);
     let borderRight;
     if (transaction.amount >= 0) {
         borderRight = "5px solid rgb(59, 228, 59)";
-    } else  {
+    } else {
         borderRight = "5px solid red";
     }
     transactionElement.innerHTML =
         `
     <td class="start">${transaction.concept}</td>
     <td class="end" style="border-right: ${borderRight}">${transaction.amount}</td>
-    <button class="btn"onclick="removeTransaction(${transaction.id})">Borrar</button>
+    <button class="btn"onclick="askToConfirmRemoveAction(${transaction.id})">Borrar</button>
     `;
     transactionsTable.appendChild(transactionElement);
-
-
 }
 
-function removeTransaction(transactionId) {
+function askToConfirmRemoveAction(transactionId) {
     let text = "¿Estás seguro de que quieres eliminar esta transacción?";
     if (confirm(text) == true) {
         removeElement(transactionId);
@@ -123,7 +107,6 @@ function removeElement(transactionId) {
     const indexOfObject = allTransactionArray.findIndex(object => {
         return object.id === transactionId;
     });
-
     allTransactionArray.splice(indexOfObject, 1);
     updateTotals();
 }
@@ -135,7 +118,7 @@ function addTransaction() {
         amount.value = amount.value.replace(",", ".");
         if (!isNaN(parseFloat(amount.value))) {
             console.log(amount.value);
-            let id = lastId + 1
+            let id = lastTransactionId + 1
             let transaction = {
                 id: id,
                 concept: concept.value,
@@ -143,29 +126,26 @@ function addTransaction() {
             }
             allTransactionArray.push(transaction);
             localStorage.setItem('myArray', JSON.stringify(allTransactionArray));
-            lastId += 1;
+            lastTransactionId += 1;
             updateTotals(amount.value);
-            showTransaction(transaction, allTransactionArray.length);
+            drawTransaction(transaction, allTransactionArray.length);
             concept.value = "";
             amount.value = "";
         } else {
             window.alert("Tiene que ser un numero")
         }
-
-
     } else {
         window.alert("Faltan datos");
     }
-
-
 }
 
 function updateTotals(amountAdded) {
     total += parseFloat(amountAdded);
-    getTotal();
-    getTotalIncome();
     getTotalExpenses();
+    getTotalIncome();
+    getTotal();
     localStorage.setItem('myArray', JSON.stringify(allTransactionArray));
+   
 
 }
 
